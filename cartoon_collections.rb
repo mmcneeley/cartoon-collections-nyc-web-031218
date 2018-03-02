e2b766a2b9153e2260c5b53664599f64fbcc2e11
Mike McNeeley <mmcneeley@gmail.com>
2018-03-02 17:52:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |dwarf, x|
+    puts "#{x+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.collect do |item|
+    item[0].upcase
+    item << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
